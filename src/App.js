f69bf1cdc86a8af17ccfcc1014f588e077548bc4
M <mrgmckenzie@gmail.com>
2023-05-16 15:55:54 +0100

    completed extension

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [tasks, setTasks] = useState(
+    [
+    {id: 1, name:"Buy shopping", priority:"high"},
+    {id: 2, name: "Clean bathroom", priority:"low"},
+    {id: 3, name: "Car's MOT", priority:"high"}
+    ]
+  )
+
+  const [newTask, setNewTask] = useState("")
+
+  const [newPriority, setNewPriority] = useState("")
+
+  const checkPriority = (task) => {
+    if (task.priority === "high") {
+      return 
+    }
+  }
+  
+  const listOfTasks = tasks.map((task) => {
+    return (
+      <li className = {task.priority} key={task.id}>{task.name}
+      <button onClick={() => {completeTask(task.id)}}>Complete</button> {/* <------------------------------------------- */}
+      </li>
+    )
+  })
+
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const handleRadioButton = (event) => {
+    setNewPriority(event.target.value)
+  }
+
+  const addNewTask = (event) => {
+    event.preventDefault()
+    if(newTask && newPriority) {
+      const newTaskObject = {id: Date.now(), name: newTask, priority: newPriority}
+      const updatedTaskList = [...tasks, newTaskObject]
+      setTasks(updatedTaskList)
+      setNewTask("")
+      setNewPriority("")
+      document.getElementById("task-priority-high").checked = false
+      document.getElementById("task-priority-low").checked = false
+    }
+  }
+
+  const completeTask = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId)
+    setTasks(newTasks)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List</h1>
+      <form onSubmit={addNewTask}>
+        <label htmlFor="new-task-input">Add a new to-do:</label>
+        <input id="new-task-input" type="text" value={newTask} onChange={handleTaskInput}></input>
+        <label htmlFor="task-priority-high">High:</label>
+        <input id="task-priority-high" type="radio" name="task-priority" value="high" onClick={handleRadioButton}></input>
+        <label htmlFor="task-priority-low">Low:</label>
+        <input id="task-priority-low" type="radio" name="task-priority" value="low" onClick={handleRadioButton}></input>
+        <input type="submit" value="Add task"></input>
+
+      </form>
+
+      <ul>
+        {listOfTasks}
+      </ul>
+      
     </div>
   );
 }
